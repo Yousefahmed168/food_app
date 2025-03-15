5b8274e081ef1cc01fa4cf2c5a994c7720438b91
Yousefahmed168 <tourkyousef@gmail.com>
2025-03-15 04:28:11 +0200

    -TODO: favorites screen

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,9 +1,32 @@
 // screens/favorites_screen.dart
 import 'package:flutter/material.dart';
 import '../utils/constants.dart';
+import '../models/food_item.dart';
 
-class FavoritesScreen extends StatelessWidget {
+class FavoritesScreen extends StatefulWidget {
   const FavoritesScreen({super.key});
+
+  @override
+
+  // ignore: library_private_types_in_public_api
+  _FavoritesScreenState createState() => _FavoritesScreenState();
+}
+
+class _FavoritesScreenState extends State<FavoritesScreen> {
+  // Create a local list of favorites based on mockFoodItems.
+  List<FoodItem> favorites = List.from(mockFoodItems);
+
+  void removeFavorite(int index) {
+    setState(() {
+      favorites.removeAt(index);
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Removed from favorites'),
+        duration: Duration(seconds: 3),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,14 +35,19 @@ class FavoritesScreen extends StatelessWidget {
         title: const Text('Favorites'),
       ),
       body: ListView.builder(
-        itemCount: mockFoodItems.length,
+        itemCount: favorites.length,
         itemBuilder: (context, index) {
-          final item = mockFoodItems[index];
+          final item = favorites[index];
           return ListTile(
             leading: Image.asset(item.imageUrl),
             title: Text(item.name),
             subtitle: Text('\$${item.price}'),
-            // TODO: Add remove from favorites button
+            // TODO: Add remove from favorites button(yousef mohamed)
+
+            trailing: IconButton(
+              icon: const Icon(Icons.delete, color: Colors.red),
+              onPressed: () => removeFavorite(index),
+            ),
           );
         },
       ),
